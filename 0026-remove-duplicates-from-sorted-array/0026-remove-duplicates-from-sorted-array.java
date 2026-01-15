90073faf394eb38566d7e890d9abc06edddb5236
sid <sidhantgupta2005@gmail.com>
2026-01-16 05:10:22 +0530

    Time: 1 ms (75.37%), Space: 46.8 MB (28.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,51 @@
 class Solution{
     public int removeDuplicates(int[] nums){
-         int i =0;
-         int j = i+1;
-         int unique = 1;
-         while(i<nums.length-1 && j<nums.length){
-            if(nums[j] != nums [j-1]){
-                nums[i+1]=nums[j];
-                i++;
-                j++;
-                unique++;
-                continue;
+        int i =0 ;
+        int j =1 ;
+        int uniq = 1;
+        int n = nums.length;
+        while(i<n-1 && j<n){
+            if (nums[j]!=nums[j-1]){
+                nums[i+1] = nums[j];
+                i++;     // i only increase when unique elem found
+                uniq++;
             }
-            j++;    //i placed at unique elem & only increases when unique elem found.
-        }
-        return unique;
+            j++;     //j increases in every condition
+        }  
+        return uniq;      
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+//     public int removeDuplicates(int[] nums){
+//          int i =0;
+//          int j = i+1;
+//          int unique = 1;
+//          while(i<nums.length-1 && j<nums.length){
+//             if(nums[j] != nums [j-1]){
+//                 nums[i+1]=nums[j];
+//                 i++;
+//                 j++;
+//                 unique++;
+//                 continue;
+//             }
+//             j++;    //i placed at unique elem & only increases when unique elem found.
+//         }
+//         return unique;
+//     }
+    
+// }

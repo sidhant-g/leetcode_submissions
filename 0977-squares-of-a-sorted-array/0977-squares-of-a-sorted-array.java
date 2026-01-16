31b252f6da9069e011f11571a190d0b4b3836116
sid <sidhantgupta2005@gmail.com>
2026-01-17 04:37:03 +0530

    Time: 7 ms (41.78%), Space: 48 MB (20.14%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,12 +1,47 @@
+// optimal
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        int j = 0;
+        int i = 0;
         int n = nums.length;
-        int[] sqNums  = new int[n];
-        for (int i =0 ; i<n ; i++){
-            sqNums[i] = nums[i]*nums[i];
+        List<Integer> nega = new ArrayList<>();
+        List<Integer> posi = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        
+        while (i < n) {   // fixed loop condition
+            if (nums[i] < 0) {
+                nega.add(nums[i] * nums[i]);
+            } else {
+                posi.add(nums[i] * nums[i]);
+            }
+            i++;
         }
-        Arrays.sort(sqNums);
-        return sqNums;
-
+        
+        i = 0;
+        Collections.reverse(nega);
+        
+        while (i < nega.size() && j < posi.size()) {
+            if (nega.get(i) < posi.get(j)) {
+                result.add(nega.get(i));
+                i++;
+            } else {
+                result.add(posi.get(j));
+                j++;
+            }
+        }     
+        
+        while (j < posi.size()) {
+            result.add(posi.get(j));
+            j++;
+        }
+        
+        while (i < nega.size()) {
+            result.add(nega.get(i));
+            i++;
+        }
+        
+        // convert List<Integer> to int[]
+        int[] resArray = result.stream().mapToInt(Integer::intValue).toArray();
+        return resArray;
     }
 }

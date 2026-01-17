c36bcbca31f348888c1161dfb32a655f815630f5
sid <sidhantgupta2005@gmail.com>
2026-01-17 23:11:07 +0530

    Time: 0 ms (100%), Space: 43.9 MB (31.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,51 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0;
-        int total = m+n;
-        while (i<n){
-            nums1[m]=nums2[i];
-            m++;
-            i++;            
-        }
-        int temp;
-        i  =0 ;
+        int i = m - 1;
+        int j = n - 1;
+        int idx = m + n - 1;
+        while (i>=0 && j>=0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[idx] = nums1[i];
+                i--;
+                idx--;
+            } else {
+                nums1[idx] = nums2[j];
+                j--;
+                idx--;
+            }
 
-        while(i<total-1){
-             if(nums1[i]>nums1[i+1]){
-                temp = nums1[i+1];
-                nums1[i+1] = nums1[i];
-                nums1[i] = temp;
-                i =0;
-                continue;
-             }i++;
+        }
+        while (j>=0){            //elem finish in nums1. put elem of j in nums1
+            nums1[idx] = nums2[j];
+            j--;
+            idx--;
+        }
+        while(i>=0){            //elem finish in nums2. put elem of nums1 in nums1
+            nums1[idx] = nums1[i];
+            i--;
+            idx--;
         }
     }
 }
+
+//         int i=0;
+//         int total = m+n;
+//         while (i<n){
+//             nums1[m]=nums2[i];
+//             m++;
+//             i++;            
+//         }
+//         int temp;
+//         i  =0 ;
+
+//         while(i<total-1){
+//              if(nums1[i]>nums1[i+1]){
+//                 temp = nums1[i+1];
+//                 nums1[i+1] = nums1[i];
+//                 nums1[i] = temp;
+//                 i =0;
+//                 continue;
+//              }i++;
+//         }
+//     }
+// }

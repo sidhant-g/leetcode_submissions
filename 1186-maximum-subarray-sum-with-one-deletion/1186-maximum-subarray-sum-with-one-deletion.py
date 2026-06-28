d0b8c074eef481f5af6e4e0da72204cf6c3e9a60
sid <sidhantgupta2005@gmail.com>
2026-06-28 17:39:55 +0530

    Time: 32 ms (27.37%), Space: 27.8 MB (79.4%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -3,14 +3,15 @@ class Solution:
         n=len(arr)
         res=arr[0]
         nodelete=arr[0]
-        onedelete=float('-inf')
+        onedelete=float('-inf') #bestending if one already deleted
         
         for i in range (1,n):
             prev_nodelete = nodelete    #store prev nodelete for curr iteration
             prev_onedelete=onedelete    #store prev onedelete for curr iteration
             nodelete = max(arr[i], prev_nodelete+arr[i])    #can contain either num itself or prevnodelete+num
             if prev_onedelete == float('-inf'): #no number deleted
-                v2 = arr[i] #at i=1 only 2 elem we have so if we delete the nums[0] then we have nums[i] only
+                v2 = arr[i] #if nothing deleted then only we can keep arr[i] in onedelete 
+                            #v2=arr[i] means we are starting at arr[i]
             else:
                 v2 = prev_onedelete + arr[i]
             onedelete = max(v2, prev_nodelete)  #prevnodelete bcz if we delete current elem then prevnodelete is the value

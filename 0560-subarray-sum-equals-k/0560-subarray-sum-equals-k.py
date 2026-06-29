2949d525dff1ebab6d233b963846781a0684c549
sid <sidhantgupta2005@gmail.com>
2026-06-30 03:11:25 +0530

    Time: 47 ms (11.3%), Space: 23.7 MB (6.2%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,14 +3,14 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n=len(nums)
         currsum=0
-        diff = 0
+        removesubarr_sum = 0
         res=0
         mp = defaultdict(int)
         mp[0]= 1
         for i in range(n):
             currsum+=nums[i]    
-            diff = currsum-k    #diff tells the sum of subarr that need to be chopped off from currsum to make k
-            waystoremove_diff  = mp[diff] #count/value of the diff tells us how many ways in which we can remove the subarr with sum 'diff' i.e, it tells us the total removable subarr for particular i.
-            res+=waystoremove_diff  #res give us the total number of removable subarr. 
-            mp[currsum]+=1  #update map with 
+            removesubarr_sum = currsum-k    #the sum of subarr that need to be chopped off from currsum to make k
+            subarr_toberemoved  = mp[removesubarr_sum] #count/value gives the total no of subarr that can be removed
+            res+=subarr_toberemoved  #res give us the total number of removable subarr. 
+            mp[currsum]+=1  #we have seen this currsum once so update inside map
         return res

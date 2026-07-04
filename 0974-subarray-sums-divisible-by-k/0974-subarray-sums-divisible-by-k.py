833a405b5243b7d083363f47868b1ffd90551454
sid <sidhantgupta2005@gmail.com>
2026-07-05 04:14:38 +0530

    Time: 35 ms (37.16%), Space: 23.8 MB (21.54%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -3,16 +3,17 @@ class Solution:
         n=len(nums)
         res = 0
         remainder = 0
-        num_with_same_remainder = 0
+        any_prefixsum_with_same_remainder = 0
         currsum = 0
-        mp = defaultdict(int)
+        mp = defaultdict(int) #map keeps (remainder->count of that remainder in the subarray)
         mp[0] = 1
         for i in range(n):
             currsum+=nums[i]
-            if nums[i]<0:
-                remainder = -((abs(nums[i])%k))%k
-            remainder = currsum % k
-            num_with_same_remainder = mp[remainder]
-            res+=num_with_same_remainder
+            if currsum<0:
+                remainder = (-(abs(currsum)%k)+k)%k #1st compute remainder as if its +ve ; make the remainder-ve; add k & again modulo with k
+            else: 
+                remainder = currsum % k
+            any_prefixsum_with_same_remainder = mp[remainder] #this tells us if any prev prefix has same remainder if,yes; remove it and count of that remainder tell that how many subarr have that same remainder
+            res+=any_prefixsum_with_same_remainder
             mp[remainder]+=1
         return res

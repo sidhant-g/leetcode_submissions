5cb8edd30ff83e14553c30c621836e4b0f2f55d4
sid <sidhantgupta2005@gmail.com>
2026-07-05 15:38:04 +0530

    Time: 1 ms (45.5%), Space: 19.3 MB (65.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,6 +2,8 @@ class Solution:
     def isValid(self, s: str) -> bool:
         n= len(s)
         stack = []
+        if len(s)%2 != 0:
+            return False
         for char in s:
             if not stack:   #if stack empty but string iteration not finished yet then; append the char.
                 stack.append(char)
@@ -9,14 +11,14 @@ class Solution:
             top = stack[-1]
             if char==')' and top == '(':
                 stack.pop()
-                continue
-            if char=='}' and top == '{':
+                
+            elif char=='}' and top == '{':
                 stack.pop()
-                continue
-            if char==']' and top == '[':
+            
+            elif char==']' and top == '[':
                 stack.pop()
-                continue
-            stack.append(char)
+            else:
+                stack.append(char)
         if not stack:   #stack=empty means all brackets in correct order
             return True
         return False    #if stack not empty; then not correct order or not enough correct brackets

48aee3acdeaabe740f7092517611199eff940121
sid <sidhantgupta2005@gmail.com>
2026-07-07 02:20:06 +0530

    Time: 23 ms (61.99%), Space: 21.2 MB (19.96%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -2,11 +2,12 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n=len(nums)
         res = [-1] * n
-        stack = []
+        stack = []  #stores indices of elem whose next greater is not found yet
         mp = defaultdict(int)
         for i in range (0, 2*n):    #iterating in circular array; us len as 2*n and access idx using i%n
             while stack and nums[stack[-1]] < nums[i%n]:  #ith elem is greater
                 res[stack[-1]] = nums[i%n]  
                 stack.pop()
-            stack.append(i%n)   #append indices when either stack empty or nums[i] <= nums(stack[-1])  
+            if i<n:  # to avoid multiple indices iteration and avoid duplicates
+                stack.append(i%n)   #append indices when either stack empty or nums[i] <= nums(stack[-1])  
         return res

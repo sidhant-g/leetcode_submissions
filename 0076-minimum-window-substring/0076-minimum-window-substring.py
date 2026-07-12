d5e7585f83a5688f46cf2465856b07039817a8e7
sid <sidhantgupta2005@gmail.com>
2026-07-12 15:17:55 +0530

    Time: 55 ms (91.77%), Space: 19.7 MB (73.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        have = [0]*256  #array of curr values
-        needed = [0]*256 #arr of required values 
-        high=0
-        low=0
-        start = low #starting index of the valid substr
-        res = float('inf')  #final len of valid substr
-        required = len(t) #this tells us abt. how many char.still left 
-
-        for c in t: #forming needed from str t                
-            needed[ord(c)]+=1                    
-        while high<len(s):
-            #window formation
-            have[ord(s[high])]+=1   #ord() returns ordinal val i.e, ASCII val for ASCII characters.
-            if have[ord(s[high])]<= needed[ord(s[high])]: 
-                required-=1
-            #if have>needed then no need to do reqd-=1 bcz the requirement for this char is already satisfied.
-            while required == 0:    # means no more char reqd Valid substr found 
-                if (high-low+1) < res:  #currLen of crctsubst<res then update res
-                    res=high-low+1
-                    start=low
-                have[ord(s[low])] -= 1
-                if have[ord(s[low])]<needed[ord(s[low])]:   #invalid substr
-                    required+=1
-                low+=1
+        low = 0
+        reqd = len(t)
+        start = low
+        res = float('inf')
+        need = defaultdict(int) # maps char to their required occurences
+#need tells us how many more char are required so when we add a char to our window simply decrement the count of that char and when we remove a char from our window increment the count of that char
+        for i in t :
+            need[i] += 1
+        for high in range (0, len(s)):
+            if need[s[high]] > 0:  # requirement of ch not currently fulfilled
+                reqd -= 1
+            need[s[high]] -= 1  #always do bcz it helps in window shrinking later
+            while reqd == 0: # valid substr containing all reqd char found
+                # as valid substr found so now we try to find the min valid substr
+                if (high-low+1) < res: # store the len if curr valid substr len < old valid substr len
+                    res = high -low+1
+                    start = low
+                #start window shrinking
+                need[s[low]] +=1
+                low+=1   # low char may or maynot be part of reqd char
+                if need[s[low-1]] > 0: # s[low] is a reqd char
+                    reqd+=1         # bcz we removed a reqd char
             high+=1
-        if res==float('inf'):
+        if res == float('inf'):
             return ""
-        return s[start:start+res]
+        return s[start: start+res]

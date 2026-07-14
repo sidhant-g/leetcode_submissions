455ed778191e1213875980ce24cb16a60a01f8c2
sid <sidhantgupta2005@gmail.com>
2026-07-14 14:46:21 +0530

    Time: 0 ms (100%), Space: 20.5 MB (89.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,17 +1,34 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        low = 0
-        high = len(nums) -1
-        while low<=high:
-            mid  = (low+high) // 2
-            if nums[mid] == target:
-                while nums[low]!= target:
-                    low+=1
-                while nums[high]!=target:
-                    high-=1
-                return [low,high]
-            elif nums[mid] < target:
-                low = mid+1
-            else:
-                high = mid-1
-        return [-1,-1]
+        if not nums:
+            return [-1,-1]
+        def leftmost (nums: List[int], target: int) -> int:
+            low = 0
+            high = len(nums) -1
+            leftmost = -1
+            while low<=high:
+                mid = (low+high)//2
+                if nums[mid] < target:
+                    low = mid+1
+                elif nums[mid]> target :
+                    high = mid-1
+                else:   #nums[mid] == target
+                    leftmost = mid
+                    high=mid-1  #to check if more leftmost targets possible
+            return leftmost
+        
+        def rightmost (nums: List[int], target: int) -> int:
+            low = 0
+            high= len(nums) - 1
+            rightmost = -1
+            while low<=high:
+                mid = (low+high)// 2
+                if nums[mid] < target:
+                    low = mid+1
+                elif nums[mid] > target:
+                    high =mid-1
+                else:   #nums[mid] == target
+                    rightmost = mid
+                    low = mid+1 #to check if more rightmost targets possible
+            return rightmost
+        return [leftmost(nums, target), rightmost(nums, target)]

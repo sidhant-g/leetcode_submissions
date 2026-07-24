b97947084682953381adadfe6509b23093791ffa
sid <sidhantgupta2005@gmail.com>
2026-07-24 16:12:14 +0530

    Time: 0 ms (100%), Space: 20.5 MB (89.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,33 +2,36 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1,-1]
-        def leftmost (nums: List[int], target: int) -> int:
-            low = 0
-            high = len(nums) -1
-            leftmost = -1
-            while low<=high:
-                mid = (low+high)//2
-                if nums[mid] < target:
-                    low = mid+1
-                elif nums[mid]> target :
-                    high = mid-1
-                else:   #nums[mid] == target
-                    leftmost = mid
-                    high=mid-1  #to check if more leftmost targets possible
-            return leftmost
+        firstOccurence  = self.firstOccurence(nums, target)
+        lastOccurence = self.lastOccurence(nums, target)
+        return [firstOccurence, lastOccurence]
+
+    def firstOccurence (self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) -1
+        firstOccurence = -1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid] < target:
+               low = mid+1
+            elif nums[mid]> target :
+                high = mid-1
+            else:   #nums[mid] == target
+                firstOccurence = mid
+                high=mid-1  #to check if more firstOccurence targets possible
+        return firstOccurence
         
-        def rightmost (nums: List[int], target: int) -> int:
-            low = 0
-            high= len(nums) - 1
-            rightmost = -1
-            while low<=high:
-                mid = (low+high)// 2
-                if nums[mid] < target:
-                    low = mid+1
-                elif nums[mid] > target:
-                    high =mid-1
-                else:   #nums[mid] == target
-                    rightmost = mid
-                    low = mid+1 #to check if more rightmost targets possible
-            return rightmost
-        return [leftmost(nums, target), rightmost(nums, target)]
+    def lastOccurence (self, nums: List[int], target: int) -> int:
+        low = 0
+        high= len(nums) - 1
+        lastOccurence = -1
+        while low<=high:
+            mid = (low+high)// 2
+            if nums[mid] < target:
+                low = mid+1
+            elif nums[mid] > target:
+                high =mid-1
+            else:   #nums[mid] == target
+                lastOccurence = mid
+                low = mid+1 #to check if more lastOccurence targets possible
+        return lastOccurence

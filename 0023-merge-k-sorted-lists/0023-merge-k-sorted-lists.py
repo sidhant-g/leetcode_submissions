c5a79af385f0d9e221a46fb002b77c0ac18329b1
sid <sidhantgupta2005@gmail.com>
2026-08-05 23:33:06 +0530

    Time: 12 ms (35.99%), Space: 22.7 MB (69.56%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,16 +4,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
+        for i in range(0, len(lists)):  #store the head of each list in the heap
+            if lists[i]:    #not empty list
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
         dummy = ListNode(0)
-        tail = dummy
-        for head in lists:
-            curr = head
-            while curr:
-                heapq.heappush(heap, curr.val)
-                curr = curr.next
+        temp = dummy
         while heap:
-            tail.next = ListNode(heapq.heappop(heap))
-            tail = tail.next
+            val, i, node = heapq.heappop(heap)  #storing i in case val of 2 elem equal then 
+                                                #node comparision gives error so i is tie-breaker
+            temp.next = node   
+            temp = temp.next    #now temp points to the newly inserted node
+            if node.next!= None:   #we insert (in heap) the next node of the node that we popped
+                heapq.heappush(heap, (node.next.val, i, node.next)) # node was the elem that we popped from heap
         return dummy.next
